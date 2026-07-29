c3103103e2ccb34cc0391af047d9513e5d92fc7a
Yoyo <159142412+Yoyodotpy@users.noreply.github.com>
2026-07-29 13:13:36 -0400

    started lexer

--- src/main.go
+++ src/main.go
@@ -9,5 +9,35 @@ var file_text_unprocessed = ""
 
 func main() {
 	file_text_unprocessed = read_file(os.Args[1])
-	fmt.Println(file_text_unprocessed)
+	lexed := lexer(file_text_unprocessed)
+	fmt.Println(lexed)
+}
+
+func lexer(code string) []string {
+	var lexed []string
+	var var_name []rune
+
+	flush := func() {
+		if len(var_name) != 0 {
+			lexed = append(lexed, string(var_name))
+			var_name = nil
+		}
+	}
+
+	for _, char := range code {
+		switch char {
+		case '(', ')', '[', ']', ',', '\'', ':', '\n':
+			flush()
+			lexed = append(lexed, string(char))
+		case '\r', ' ', '\t':
+			flush()
+			//ignore \r to stop potential problems on windows
+		default:
+			var_name = append(var_name, char)
+		}
+	}
+
+	flush()
+
+	return lexed
 }
